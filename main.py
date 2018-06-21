f062ef903c066f4c1e2d8d75ed9a4acf1d210bdf
k0305916 <k0305916@users.noreply.github.com>
2018-06-21 17:01:22 +0800

    framework is OK.
    1. the detail about algorithm need to rethink.

--- main.py
+++ main.py
@@ -20,6 +20,8 @@
 # 234
 # 345
 # 0345
+import operator
+import collections
 
 
 def Init():
@@ -40,17 +42,33 @@ def Init():
 
 
 def GetStringArrayPossibility(text, dic):
-    result = []
-    while len(text) > 0:
-        for c in text:
-            pass
-    return result
+    result = {}
+    for c in text:
+        value = dic[c]
+        for item in value:
+            if item not in result:
+                result[item] = 1
+            else:
+                result[item] += 1
+    print(result)
+    # get result max--python3
+    # max(result.items(), key=operator.itemgetter(1))[0]
+    compareresult = {k: v for k, v in result.items() if (len(k) <= v) and 
+    (len(set(k) & set(text)) == len(set(k)))}.keys()
+    print(compareresult)
+    return compareresult
 
 
 def GetNumber(text):
-    dic = {'ONE': 1, 'Two': 2, 'THREE': 3, 'FOUR': 4, 'FIVE': 5, 'SIX': 6, 'SEVEN': 7, 'EIGHT': 8,
+    result = []
+    dic = {'ONE': 1, 'TWO': 2, 'THREE': 3, 'FOUR': 4, 'FIVE': 5, 'SIX': 6, 'SEVEN': 7, 'EIGHT': 8,
            'NINE': 9, 'ZERO': 0}
-    return dic[text]
+    # if isinstance(number,collections.Iterable):
+    for item in text:
+        result.append(dic[item])
+    # else:
+    #     result.append(dic[text])
+    return result
 
 
 def GetMinSequence(number):
@@ -61,16 +79,22 @@ def GetOriginalNumber(number):
     original = []
     for num in number:
         original.append((10 + (num - 8)) % 10)
+    # return ''.join(''.join('%s' % id for id in original))
     return original
+
 
 initdic = Init()
 print(initdic)
 number = int(input("Input number please: "))
 for i in range(number):
-    text = input('Input string please: ')
-    possbilityArray = GetStringArrayPossibility(text, initdic)
-    possbilityNumber = GetNumber(possbilityArray)
-    possbilityMinNumber = GetMinSequence(possbilityNumber)
-    possibilityOriginNumber = GetOriginalNumber(possbilityMinNumber)
-    result = ''.join(possibilityOriginNumber)
-    print(result)
+    text = input('Input string please: ').upper()
+    possbilityvalue = GetStringArrayPossibility(text, initdic)
+    possbilityNumber = GetNumber(possbilityvalue)
+    possibilityOriginNumber = GetOriginalNumber(possbilityNumber)
+    result = GetMinSequence(possibilityOriginNumber)
+    
+    # if len(possibilityOriginNumber) <= 1:
+    #     result = possibilityOriginNumber[0]
+    # else:
+    #     result = ' '.join(possibilityOriginNumber)
+    print(''.join('%s' % id for id in result))
