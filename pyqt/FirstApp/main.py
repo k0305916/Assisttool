7ad9693c108add5f71c66e9d9c0f9b264e1ac90c
李繁 <lifan@Mr-Li-iMac.local>
2018-08-25 10:07:58 +0800

    note: 1. ui file does not clude solts and signle, should define by self; 2. controls define should be written after self.setupUi(self).

--- pyqt/FirstApp/main.py
+++ pyqt/FirstApp/main.py
@@ -1,14 +1,20 @@
 import sys
-from PyQt5 import QtCore, QtGui,QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from Ui_mainwindow import Ui_MainWindow
 
-class MyApp(QtWidgets.QMainWindow,Ui_MainWindow):
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
+        self.CalcBtn.clicked.connect(self.CalculateTax)
 
-
+    def CalculateTax(self):
+        price = int(self.Price_edit.toPlainText())
+        tax = float(self.TexRate_edit.toPlainText())
+        total_price = price + (tax*price)
+        total_price_str = "The total price with tax is:" + str(total_price)
+        self.FinalPriceResult.setText(total_price_str)
 
 
 if __name__ == "__main__":
